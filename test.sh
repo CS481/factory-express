cb99f7817311ade7881fdf2038ce19c4886fedeb
caydenreynolds <46609378+caydenreynolds@users.noreply.github.com>
2021-01-23 20:02:07 -0500

    Deleting at Brandon's suggestion

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export DB_USR=testuser
-export DB_PWD=952bfbp7qQue7QT
-export DB_NAME=test
-export DB_HOST=cluster0.hgu8w.mongodb.net
-./node_modules/.bin/babel src --out-dir lib
-node node_modules/jest/bin/jest.js lib
