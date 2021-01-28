10cd1fa27b4d0d4695c2ceb8623e2e7312bc502b
caydenreynolds <cayden@arclasr.net>
2021-01-28 14:51:46 -0500

    Added Router class to reduce boilerplate

---
+++ src/routes/VerifyCredentials.js
@@ -0,0 +1,12 @@
+import User from '../model/User.js';
+import Router from "../Router.js";
+import UserSchema from "../simulation-schema/js/User.js";
+import VoidSchema from "../simulation-schema/js/Void.js";
+
+var router = new Router("VerifyCredentials", UserSchema, VoidSchema);
+router.post(async function(req) {
+    await new User().fromJsonObject(req);
+    return {};
+});
+
+export default router;
