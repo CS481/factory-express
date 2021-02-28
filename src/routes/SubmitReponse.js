cb49c1272074515d259f90828c476cbf117f1f19
austin canzano <acanzano@ycp.edu>
2021-02-28 13:18:02 -0500

    Dummysubmit

---
+++ src/routes/SubmitReponse.js
@@ -0,0 +1,19 @@
+import User from '../model/User.js';
+import Frame from '../model/Frame.js';
+import Router from "../Router.js";
+import SimulationInstance from '../model/SimulationInstance.js';
+import UserResponse from "../simulation-schema/js/UserResponse.js";
+import IdRequest from "../simulation-schema/js/IdRequest.js";
+import IdResponse from '../simulation-schema/js/IdResponse.js';
+
+var router = new Router("SubmitReponse", UserResponse, State);
+router.post(async function(req) {
+    let user = await new User().fromJsonObject(req.user);
+    delete req.user; // Remove user from the message so we don't override it
+    let simulation = new Simulation();
+    let State = await simulation.getState();
+    return {State: State}; 
+
+});
+
+export default router;
