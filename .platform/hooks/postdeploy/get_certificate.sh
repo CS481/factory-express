aac5be3a81135611813f69376dd382255e2c0a9b
Cayden <cayden@arclasr.net>
2021-04-18 13:48:18 -0400

    Better certbot command

--- .platform/hooks/postdeploy/get_certificate.sh
+++ .platform/hooks/postdeploy/get_certificate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo certbot -n -d simulationfactory.zapto.org -d simulationfactory-env.eba-ybnn4zdk.us-east-2.elasticbeanstalk.com --nginx --agree-tos
+sudo certbot -n -d simulationfactory.zapto.org -d simulationfactory-env.eba-ybnn4zdk.us-east-2.elasticbeanstalk.com --nginx --agree-tos --expand
