ad333328aca0a4a391823233abddf641c2a5e8f2
Robert Doster <rdoster@ycp.edu>
2021-05-04 14:55:24 -0400

    Actually Fixed it.

--- src/model/SimulationInstance.js
+++ src/model/SimulationInstance.js
@@ -5,6 +5,7 @@ import State from "./State.js"
 import StateHistory from "./StateHistory.js";
 import User from "./User.js";
 import BadRequestError from "../exception/BadRequestError.js";
+import UnauthorizedError from "../exception/UnauthorizedError.js";
  
 export default class SimulationInstance extends SimObj {
     tablename = "SimulationInstances";
@@ -209,13 +210,22 @@ export default class SimulationInstance extends SimObj {
         */
         this.user_count = {"$lt": simulation.user_count};
         let instances = await this.selectMany();
-       
+        // Set the same player to be false to try to begin sim. 
+        let same_player = false;
+
         if (await instances.length == 0) {
             return await this._new_sim_instance(user, simulation);
         } else {
-            if (instances[0].player_responses.includes(user)) {
+            // If simInst is already created, Check the entries
+            await instances[0].player_responses.forEach(user_elm =>{
+                //If an entry is the same, set the variable to true. 
+                same_player = user_elm.user == user.id;
+            } );
+
+            // If same player exists (set to true) throw error. 
+            if (same_player == true){
                 throw new UnauthorizedError("User aready joined this instance");
-                }
+            } 
             await instances[0]._add_to_sim_instance(user, simulation);
             return this.id;
         }
