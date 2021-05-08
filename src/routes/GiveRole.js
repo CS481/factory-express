fbf33db4724435db9139818c0c03b26c8a1f2861
austin canzano <acanzano@ycp.edu>
2021-05-08 15:12:26 -0400

    Fixed spelling

--- src/routes/GiveRole.js
+++ src/routes/GiveRole.js
@@ -1,9 +1,9 @@
 import User from '../model/User.js';
 import Router from "../Router.js";
-import GiveRollSchema from "../simulation-schema/js/GiveRoll.js";
+import GiveRoleSchema from "../simulation-schema/js/GiveRole.js";
 import VoidSchema from "../simulation-schema/js/Void.js";
 
-var router = new Router("GiveRoll", GiveRollSchema, VoidSchema);
+var router = new Router("GiveRole", GiveRoleSchema, VoidSchema);
 router.post(async function(req){}
     let user = await new User().fromJsonObject(req.user);
     delete req.user; // Remove user from the message so we don't override it in modify_sim{
